ec0496e37b11679f32d4b9d3c64e5178359900af
Radoslav Georgiev <rudix.lab@gmail.com>
2022-12-20 21:20:24 +0000

    xxx

--- src/pages/api/img/[id].tsx
+++ src/pages/api/img/[id].tsx
@@ -1,11 +1,6 @@
 import { ImageResponse } from "@vercel/og";
 import { NextRequest } from "next/server";
-import { remappedJokeFunction } from "../../../components/JokeText";
 import { profanityFilter } from "../../../utils/formatter";
-
-export const config = {
-  runtime: "edge",
-};
 
 export default async function handler(req: NextRequest) {
   try {
@@ -30,58 +25,7 @@ export default async function handler(req: NextRequest) {
             flexDirection: "column",
           }}
         >
-          {JSON.stringify(req)}
-          {remappedJokeFunction(joke).map(
-            ({
-              oddness,
-              line,
-              key,
-            }: {
-              oddness?: string;
-              line: string;
-              key: number;
-            }) =>
-              oddness ? (
-                <div
-                  key={key}
-                  style={{
-                    display: "flex",
-                  }}
-                >
-                  <div
-                    style={{
-                      display: "flex",
-                    }}
-                  >
-                    {oddness === "odd" ? (
-                      <div
-                        className='font-5xl'
-                        style={{
-                          display: "flex",
-                        }}
-                      >
-                        od {line}
-                      </div>
-                    ) : (
-                      <div
-                        style={{
-                          display: "flex",
-                          color: "red",
-                          fontSize: 40,
-                        }}
-                        className='pl-2'
-                      >
-                        oven {line}
-                      </div>
-                    )}
-                  </div>
-                </div>
-              ) : (
-                <div key={key} className='block pb-4 text-lg flex font-5xl'>
-                  {line}
-                </div>
-              )
-          )}
+          test
         </div>
       ),
       {
