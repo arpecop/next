a00c5b0bea2067b1d5881388ec28acf98a8e4f45
Radoslav Georgiev <rudix.lab@gmail.com>
2022-12-22 12:55:13 +0000

    xxx

--- src/components/hooks/facebook.ts
+++ src/components/hooks/facebook.ts
@@ -1,32 +1,46 @@
+import { nanoid } from "nanoid";
 import { useEffect, useState } from "react";
 import { FbApp } from "../../pages/facebook/facebook";
 import { getCookie, setCookie } from "../../utils/cookies";
+export type FBResult = {
+	[key: string]: string | undefined | boolean;
+};
 
 export function useFacebookRandom(app?: FbApp) {
-	const [rditem, setRditem] = useState<number>(-1);
+	const [selectedapp, setSelectedApp] = useState<FbApp | undefined>();
 	const [seen, setSeen] = useState<boolean>(true);
+	const [item, setItem] = useState<FBResult>({
+		id: nanoid(5),
+		type: "facebook",
+		cat: app?.slug,
+	});
+
+	useEffect(() => {
+		setSelectedApp(app);
+	}, []);
 	useEffect(() => {
 		const rdcoki = getCookie(app?.slug || "main");
-		const seen = getCookie("seen");
-		setSeen(seen ? true : false);
-		setRditem(Number(rdcoki));
+
+		//setRditem(JSON.parse(rdcoki || "{}"));
 		const chooseRandomJustIncase = async () => {
 			const res2 = await fetch(`/facebook/${app?.slug}/items.json`);
-
 			const data = await res2.json();
-			console.log(typeof data, data.length);
-
 			const index = Math.floor(Math.random() * data.length);
-			setCookie(app!.slug, index);
-			setRditem(index);
+			setCookie(app!.slug, JSON.stringify({ index, ...data[index] }));
+			setItem({ ...item, index, ...data[index], seen });
 		};
 
-		if (app?.cat && !rdcoki) {
+		if (selectedapp && !rdcoki) {
 			chooseRandomJustIncase();
+		} else {
+			setItem({ ...item, ...JSON.parse(rdcoki || "{}") });
+			console.log("already have refreshing");
 		}
-	});
-	if (!app) {
-		return { rditem: -1, seen };
-	}
-	return { rditem, seen };
+	}, [selectedapp]);
+
+	useEffect(() => {
+		console.log(item);
+	}, [item]);
+
+	return item;
 }
--- src/pages/facebook/facebook.tsx
+++ src/pages/facebook/facebook.tsx
@@ -1,11 +1,9 @@
-import { nanoid } from "nanoid";
 import type { GetServerSideProps } from "next";
 
 import FacebookShare from "@/components/FacebookShare";
 import Main from "@/components/Layouts/Main";
 import Meta from "@/components/Layouts/Meta";
 import Nav from "@/components/Nav";
-import { useEffect } from "react";
 
 import Cookies from "../../components/Cookies";
 import { useFacebookRandom } from "../../components/hooks/facebook";
@@ -19,30 +17,66 @@ export type FbApp = {
 	isLoginRequired?: boolean;
 };
 
+const apps = [
+	{
+		cat: "Баница с късмети за коледа 2022",
+		slug: "banicak2022",
+		count: 2022,
+		controls: ["preresult"],
+	},
+	{ cat: "Новогодишна Баница 2023", slug: "banica2023", count: 2023 },
+	{
+		cat: "На колко години изглеждаш",
+		slug: "godini",
+		button: "Провери на колко изглеждаш",
+		isLoginRequired: true,
+	},
+	{
+		cat: "Провери значението на името си",
+		slug: "assbg_BG",
+	},
+	{
+		cat: "Добър или лош си днес",
+		slug: "goodbad",
+		isLoginRequired: true,
+	},
+	{
+		cat: "На коя известна личност приличаш",
+		slug: "celeb",
+	},
+	{
+		cat: "Изтегли си късметче",
+		slug: "iztegli",
+	},
+	{
+		cat: "Индиянското ти име",
+		slug: "indianskoime",
+	},
+	{
+		cat: "Виж коя известна личност ти подхожда",
+		slug: "podhojda",
+	},
+	{ cat: "Какво е японското ти име", slug: "iaponskoime" },
+	{ cat: "Провери какъв си бил в предишен живот", slug: "predishenjivot" },
+];
+
 const Facebook = ({
 	cats,
 	result,
-	app,
+	appid,
 }: {
 	cats: FbApp[];
-	result?: { appid: string; id: string; title: string; description: string };
-	app?: FbApp;
+	result?: {
+		appid: string;
+		id: string;
+		title: string;
+		description: string;
+		image: string;
+	};
+	appid?: string;
 }) => {
-	const rditem = useFacebookRandom(app);
-
-	useEffect(() => {
-		const shid = nanoid(5);
-		if (rditem.rditem >= 0) {
-			const result = {
-				id: shid,
-				...app,
-				result: rditem.rditem,
-				type: "facebook",
-				cat: app?.slug,
-			};
-			console.log(result);
-		}
-	}, [rditem]);
+	const app = apps.find((app) => app.slug === appid) as FbApp | undefined;
+	const preresult = useFacebookRandom(app);
 
 	return (
 		<Main
@@ -50,29 +84,34 @@ const Facebook = ({
 				<Meta
 					title={result?.title || app?.cat || "Фейсбук приложения"}
 					description={result?.description || "Фейсбук приложения"}
+					image={result?.image}
 				/>
 			}
 		>
-			<div className='flex  justify-center items-center  rounded-md px-2 mx-auto w-full'>
-				{app && (
-					<div>
-						{!app.button ? (
-							<div>
-								<h1 className='text-5xl font-thin text-center'>{app.cat}</h1>
-							</div>
-						) : (
-							<h1 className='text-5xl font-thin text-center'>{app.cat}</h1>
-						)}
-
-						<FacebookShare text={app?.button} />
+			{preresult.index && (
+				<div className='container mx-auto flex  justify-center items-center flex-col'>
+					<div className='absolute container'>
+						<h1 className='text-4xl md:text-5xl font-thin text-center'>
+							{JSON.stringify(preresult.index)}
+						</h1>
 					</div>
-				)}
-			</div>
+
+					<svg
+						className='w-full h-full'
+						width='1200'
+						height='630'
+						viewBox='0 0 1200 630'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'
+					/>
+					<FacebookShare text={app?.button} />
+				</div>
+			)}
 			<Nav cats={cats} prefix='fb' />
 			<div className='my-10 flex w-full flex-col'>
 				<div className='flex flex-wrap' />
 			</div>
-			{rditem.seen === false && <Cookies />}
+			{!preresult.seen && <Cookies />}
 		</Main>
 	);
 };
@@ -80,52 +119,10 @@ const Facebook = ({
 export const getServerSideProps: GetServerSideProps = async (context) => {
 	const { appid } = context.query;
 
-	const cats = [
-		{
-			cat: "Баница с късмети за коледа 2022",
-			slug: "banicak2022",
-			count: 2022,
-		},
-		{ cat: "Новогодишна Баница 2023", slug: "banica2023", count: 2023 },
-		{
-			cat: "На колко години изглеждаш",
-			slug: "godini",
-			button: "Провери на колко изглеждаш",
-			isLoginRequired: true,
-		},
-		{
-			cat: "Провери значението на името си",
-			slug: "assbg_BG",
-		},
-		{
-			cat: "Добър или лош си днес",
-			slug: "goodbad",
-			isLoginRequired: true,
-		},
-		{
-			cat: "На коя известна личност приличаш",
-			slug: "celeb",
-		},
-		{
-			cat: "Изтегли си късметче",
-			slug: "iztegli",
-		},
-		{
-			cat: "Индиянското ти име",
-			slug: "indianskoime",
-		},
-		{
-			cat: "Виж коя известна личност ти подхожда",
-			slug: "podhojda",
-		},
-		{ cat: "Какво е японското ти име", slug: "iaponskoime" },
-		{ cat: "Провери какъв си бил в предишен живот", slug: "predishenjivot" },
-	];
-
 	return {
 		props: {
-			cats: cats.filter((cat) => cat.slug !== appid),
-			app: cats.find((x) => x.slug === appid) || null,
+			cats: apps.filter((cat) => cat.slug !== appid),
+			appid: appid || null,
 		},
 	};
 };
