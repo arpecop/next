c22d6d580e0936009952f3885c653aae9a351117
Radoslav Georgiev <rudix.lab@gmail.com>
2022-12-29 16:01:11 +0000

    xxx

--- src/components/Layouts/Main.tsx
+++ src/components/Layouts/Main.tsx
@@ -12,27 +12,14 @@ type IMainProps = {
 };
 
 const Main = (props: IMainProps) => {
-	useEffect(() => {
-		// rome-ignore lint/suspicious/noExplicitAny: <explanation>
-		let adsbygoogle: any;
-		const ads = document.getElementsByClassName("adsbygoogle").length as number;
-		for (let i = 0; i < ads; i++) {
-			try {
-				// rome-ignore lint/suspicious/noExplicitAny: <explanation>
-				(adsbygoogle = (window as any).adsbygoogle || []).push({});
-			} catch (e) {
-				//////console.log(e);
-			}
-		}
-	}, []);
 	return (
-		<div className='flex min-h-screen flex-col'>
+		<div className="flex min-h-screen flex-col">
 			{props.meta}
 			<Header title={props.title} />
 			{props.noContainer ? (
-				<main className='flex grow flex-col z-30'>{props.children}</main>
+				<main className="flex grow flex-col z-30">{props.children}</main>
 			) : (
-				<main className='container mx-auto flex grow flex-col justify-center z-30'>
+				<main className="container mx-auto flex grow flex-col justify-center z-30">
 					{props.children}
 				</main>
 			)}
