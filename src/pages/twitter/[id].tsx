31df7d7660b3c2b1c97e3450d9235603e3ab6a68
Radoslav Georgiev <rudix.lab@gmail.com>
2023-01-03 12:34:20 +0000

    xxx

--- src/pages/twitter/[id].tsx
+++ src/pages/twitter/[id].tsx
@@ -5,7 +5,54 @@ import Main from "@/components/Layouts/Main";
 import Meta from "@/components/Layouts/Meta";
 import { TwitterFeed } from "../../data/twittertypes";
 
-const TwuserPage = (props: TwitterFeed): JSX.Element => {
+type Tweet = {
+	id: string;
+	text: string;
+	createdAt: string;
+	screenName: string;
+	name: string;
+	profileImageUrl: string;
+	originalPoster: {
+		screenName: string;
+		name: string;
+		profileImageUrl: string;
+	} | null;
+};
+
+const minifyTweets = (obj: TwitterFeed) => {
+	const tweets = obj.props.pageProps.timeline.entries.map((t) => {
+		const originalPoster = t.content.tweet.retweeted_status
+			? {
+				screenName: t.content.tweet.retweeted_status.user.screen_name,
+				name: t.content.tweet.retweeted_status.user.name,
+				profileImageUrl:
+					t.content.tweet.retweeted_status.user.profile_image_url_https,
+			}
+			: null;
+		return {
+			id: t.entry_id,
+			text: t.content.tweet.retweeted_status
+				? t.content.tweet.retweeted_status.full_text
+				: t.content.tweet.text,
+			createdAt: new Date(t.content.tweet.created_at)
+				.toISOString()
+				.split("T")[0],
+			screenName: t.content.tweet.user.screen_name,
+			name: t.content.tweet.user.name,
+			profileImageUrl: t.content.tweet.user.profile_image_url_https,
+			originalPoster,
+		};
+	});
+	return tweets;
+};
+
+const TwuserPage = ({
+	props,
+	tweets,
+}: {
+	props: TwitterFeed;
+	tweets: Tweet[];
+}): JSX.Element => {
 	return (
 		<Main
 			hideFooter
@@ -22,68 +69,54 @@ const TwuserPage = (props: TwitterFeed): JSX.Element => {
 				/>
 			}
 		>
-			{props.props.pageProps.timeline.entries.map((t) => (
-				<div key={t.entry_id}>
+			{tweets.map((t) => (
+				<div key={t.id}>
 					<div className="flex flex-shrink-0 p-4 pb-0">
 						<a className="flex-shrink-0 group block">
 							<div className="flex items-center">
 								<div>
 									<img
 										className="inline-block h-10 w-10 rounded-full"
-										src={t.content.tweet.user.profile_image_url_https}
+										src={t.profileImageUrl}
 										alt=""
 									/>
 								</div>
 								<div className="ml-2">
 									<p className="text-base leading-6 font-bold">
-										{t.content.tweet.user.name}
+										{t.name}
 										<span className="text-sm leading-5 font-medium  pl-2">
-											{
-												new Date(t.content.tweet.created_at)
-													.toISOString()
-													.split("T")[0]
-											}
+											{t.createdAt}
 										</span>
-										{t.content.tweet.retweeted_status ? " retweeted:" : ""}
+										{t.originalPoster ? " retweeted:" : ""}
 									</p>
 								</div>
 							</div>
 						</a>
 					</div>
 					<div>
-						{t.content.tweet.retweeted_status ? (
+						{t.originalPoster ? (
 							<a
 								className="flex  items-center"
-								href={
-									"/tw/u/" + t.content.tweet.retweeted_status.user.screen_name
-								}
+								href={"/tw/u/" + t.originalPoster.screenName}
 							>
 								<img
 									className="inline-block h-5 w-5 rounded-full ml-14"
-									src={
-										t.content.tweet.retweeted_status.user
-											.profile_image_url_https
-									}
+									src={t.originalPoster.profileImageUrl}
 									alt=""
 								/>
 								<div className="text-base leading-6 font-bold pl-2">
-									{t.content.tweet.retweeted_status.user.name} :
+									{t.originalPoster.screenName} :
 								</div>
 							</a>
 						) : (
 							<div className="h-1" />
 						)}
 
-						{(t.content.tweet.retweeted_status
-							? t.content.tweet.retweeted_status.full_text
-							: t.content.tweet.text
-						)
-							.split("\n")
-							.map((p, i) => (
-								<div key={i} className="pl-14 w-full">
-									{p}
-								</div>
-							))}
+						{t.text.split("\n").map((p, i) => (
+							<div key={i} className="pl-14 w-full">
+								{p}
+							</div>
+						))}
 					</div>
 				</div>
 			))}
@@ -110,10 +143,13 @@ export const getServerSideProps = async ({ query }: { query: { id: string } }) =
 			.split("<script>")[1]
 			.split("</script>")[0]
 	) as TwitterFeed;
-	console.log(props);
+	console.log(minifyTweets(props));
 
 	return {
-		props,
+		props: {
+			props,
+			tweets: minifyTweets(props),
+		},
 	};
 };
 
