ade912fadfb182a70b11f976f57358f53a3f93a6
Radoslav Georgiev <rudix.lab@gmail.com>
2023-01-04 12:29:17 +0000

    xxx

--- next.config.js
+++ next.config.js
@@ -58,6 +58,15 @@ const nextConfig = {
       },
     ];
   },
+  exportPathMap: async function (
+    defaultPathMap,
+    {dev, dir, outDir, distDir, buildId}
+  ) {
+    return {
+      "/": {page: "/"},
+      "/other/about": {page: "/other/about"},
+    };
+  },
 };
 
 module.exports = nextConfig;
