47aeb37bb654cce29c3a93681c236823a6cad661
Radoslav Georgiev <rudix.lab@gmail.com>
2023-01-05 10:59:47 +0000

    xxx

--- src/components/helpers/loadStaticFile.ts
+++ src/components/helpers/loadStaticFile.ts
@@ -1,16 +1,22 @@
+import { readFileSync } from "fs";
+import path from "path";
+
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const loadStaticFile = async (
   loc: string
-): Promise<{[key: string]: string | {[key: string]: any}[]}[]> => {
+): Promise<{ [key: string]: string | { [key: string]: any }[] }[]> => {
   const url =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/api/data/"
-      : "http://localhost:3000/api/data/";
-  const data = await fetcher(url + "" + loc);
+    "https://raw.githubusercontent.com/arpecop/next/eziktok/public/data/";
+  // const data = await fetcher(url + "" + loc);
+
+  const rootfolder = __dirname.split(".next")[0];
+
+  const ff = path.resolve(rootfolder, `public/data/${loc}.json`);
+  const items = JSON.parse(readFileSync(ff).toString());
 
   return new Promise((resolve) => {
-    resolve(data);
+    resolve(items);
   });
 };
 export default loadStaticFile;
