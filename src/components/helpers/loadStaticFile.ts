2f093bc17554edea1b8bc0cc1e26f68c0f448214
Radoslav Georgiev <rudix.lab@gmail.com>
2023-01-05 10:46:11 +0000

    xxx

--- src/components/helpers/loadStaticFile.ts
+++ src/components/helpers/loadStaticFile.ts
@@ -2,11 +2,11 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const loadStaticFile = async (
   loc: string
-): Promise<{ [key: string]: string | { [key: string]: any }[] }[]> => {
+): Promise<{[key: string]: string | {[key: string]: any}[]}[]> => {
   const url =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/api/data/"
-      : "https://eziktok.com/api/data/";
+      : "http://localhost:3000/api/data/";
   const data = await fetcher(url + "" + loc);
 
   return new Promise((resolve) => {
