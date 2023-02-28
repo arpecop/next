9ac1e3ee7afeef833a99c8bfd5a5dd758a48f82a
rudiX <rudix.lab@gmail.com>
2023-02-28 00:03:16 +0000

    zz

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from "next/app";
 import "../styles/other.css";
 import "../styles/global.css";
 
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,12 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       attribute="class"
       defaultTheme={"light"}
     >
-      <Script id="Adsense-id" async
-        onError={(e) => { console.error("Script failed to load", e); }}
-        strategy="afterInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5476404733919333"
-        crossOrigin="anonymous"
-      />
+
 
 
       <Component {...pageProps} />
