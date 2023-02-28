cf5bfceb023ae60d5f7349a0019bf21b457a1544
rudiX <rudix.lab@gmail.com>
2023-02-28 00:30:48 +0000

    dsad

--- src/components/Layouts/Meta.tsx
+++ src/components/Layouts/Meta.tsx
@@ -77,7 +77,6 @@ const Meta = (props: IMetaProps) => {
       />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       <Script id="Adsense-id" async
-        onError={(e) => { console.error("Script failed to load", e); }}
         strategy="afterInteractive"
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5476404733919333"
         crossOrigin="anonymous"
