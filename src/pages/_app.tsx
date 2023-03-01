545c470daf1bed481a1cac4059832cadfe909d02
rudiX <rudix.lab@gmail.com>
2023-03-01 15:04:53 +0000

    dsdsd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,15 @@
-import {ThemeProvider} from "next-themes";
-import type {AppProps} from "next/app";
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
 import "../styles/other.css";
 import "../styles/global.css";
-import Script from "next/script";
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider
       enableSystem={false}
       attribute="class"
       defaultTheme={"light"}
     >
-      <Script
-        id="Adsense-id"
-        async
-        strategy="afterInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle w-full.js?client=ca-pub-5476404733919333"
-        crossOrigin="anonymous"
-      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
