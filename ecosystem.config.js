e00ed7330c41afb36dc72af092f84cf7be62eb00
rudiX <rudix.lab@gmail.com>
2023-03-03 23:59:16 +0000

    xxx

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "build",
-      script: "yarn start --port 3001",
+      script: "npm start --port 3001",
       env: {
         NODE_ENV: "production",
       },
