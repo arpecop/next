ed3cf1dd977cf3ac0ff5c6294918fb396c41a1de
rudiX <rudix.lab@gmail.com>
2023-03-04 00:22:01 +0000

    xxxx

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'eziktokbuild',
-      script: 'yarn start --port 3002',
+      script: 'npm start --port 3002',
       env: {
         NODE_ENV: 'production',
       },
