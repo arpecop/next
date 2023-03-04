3705b01767a2db715c51f3475ea312303e58c983
rudiX <rudix.lab@gmail.com>
2023-03-04 01:38:31 +0000

    xxx

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "build",
-      script: "npm start --port 3001",
+      script: "pnpm start --port 3001",
       env: {
         NODE_ENV: "production",
       },
